f9f0a3169408eaaf202209df0718f543bb106c56
moi <tdaniel.webdev@gmail.com>
2023-11-22 21:23:04 +0100

    last fixes version 2

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -14,6 +14,8 @@ $(document).ready(function($){
             const scrollTop = window.pageYOffset+window.outerHeight/2;
             if ($(element).offset().top < scrollTop && $(element).offset().top + $(element).outerHeight() > scrollTop) {
                 $(correspondingIconId).addClass('in-section');
+                console.log(correspondingIconId)
+                console.log($(correspondingIconId).attr('class'));
             }
         });
     }, 10));
